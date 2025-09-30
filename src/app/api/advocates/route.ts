16966f12e82070e957b547929984bc8f4a42edd9
Andre LaFleur <cincospenguinos@gmail.com>
2025-09-30 09:41:58 -0600

    Swap over to use database
    * Given this is meant to represent a full application, I'd prefer to
      use docker compose along with what we've got here
    * Also, since we're going to want to implement filtering, we can have
      the DB do the search shennanigans for us, which will both be more
      performant and more robust

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -2,11 +2,8 @@ import db from "../../../db";
 import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
-
-  const data = advocateData;
+export async function GET(foo) {
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }
